78d9564c826e27dd94b95ba84cd9b8ee4bdcbea4
haappi <74637670+haappi@users.noreply.github.com>
2022-04-21 15:28:59 -0500

    something

---
+++ Cards/2_of_heart.py
@@ -0,0 +1,6 @@
+import utild
+
+
+class Two_of_hearts(utild.Card):
+    def __init__(self):
+        super().__init__(2, 'Hearts', 1)
--- utild.py
+++ utild.py
@@ -1,6 +1,12 @@
+from typing import Union
+
+
 class Card:
+    # instance = None
+    # todo singleton pattern
     def __init__(self, suite, number, color):
-        self.suite = suite  # ace / spade / heart / diamond
-        self.number = number  # 1 / 2 / 3 / 4 / 5 / 6 / 7 / 8 / 9 / 10 / jack / queen / king
+        self.suite = suite  # ace / spade / hearts / diamond
+        self.number: Union[str, int] = number  # 1 / 2 / 3 / 4 / 5 / 6 / 7 / 8 / 9 / 10 / jack / queen / king
         self.color = color  # red / black,
+        self.name = f"{self.number} of {self.suite}"
 
