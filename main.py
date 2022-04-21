1af8d44725fd1fbca468c561a8e8aae01205c589
haappi <74637670+haappi@users.noreply.github.com>
2022-04-21 15:17:30 -0500

    message

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+https://www.wikihow.com/Play-Solitaire
+   [a] [a] [a] [a]
+y x x x x x x
+  y x x x x x
+    y x x x x
+      y x x x
+        y x x
+          y x
+            y
+[]
+
+y = cards that are face up
+x = cards that are face down
+[a] = cards that are face up and are in the ace piles
+[] = cards that are face down and are in the extras pile
+
+# Make sure that the game has some sort of creative twist. Nothing that can be *easily* found on the internet.
+"""
+
+# 500 x 726
+import random
---
+++ utild.py
@@ -0,0 +1,6 @@
+class Card:
+    def __init__(self, suite, number, color):
+        self.suite = suite  # ace / spade / heart / diamond
+        self.number = number  # 1 / 2 / 3 / 4 / 5 / 6 / 7 / 8 / 9 / 10 / jack / queen / king
+        self.color = color  # red / black,
+
