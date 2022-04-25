929502c854c7f16fd94b55717c04efdb93eb40d7
haappi <74637670+haappi@users.noreply.github.com>
2022-04-25 15:16:01 -0500

    i am a bad coder

--- Cards/CardObjects/__init__.py
+++ Cards/CardObjects/__init__.py
@@ -9,15 +9,15 @@ name_mappings: typing.Final = {
 }
 
 number_mappings: typing.Final = {
-    1: "1",
-    2: "2",
-    3: "3",
-    4: "4",
-    5: "5",
-    6: "6",
-    7: "7",
-    8: "8",
-    9: "9",
+    "one:": 1,
+    "two:": 2,
+    "three:": 3,
+    "four:": 4,
+    "five:": 5,
+    "six:": 6,
+    "seven:": 7,
+    "eight:": 8,
+    "nine:": 9,
 }
 
 color_mappings: typing.Final = {
