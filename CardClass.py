e612089e37139376126bcb9165db795a7382f775
haappi <74637670+haappi@users.noreply.github.com>
2022-05-11 15:19:26 -0500

    random logic card placing

--- CardClass.py
+++ CardClass.py
@@ -9,11 +9,17 @@ class Card(arcade.Sprite):
         self.__number: typing.Union[
             str, int
         ] = number  # 1 / 2 / 3 / 4 / 5 / 6 / 7 / 8 / 9 / 10 / jack / queen / king
+        try:
+            self.__internal_number: typing.Final[int] = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10,
+                                                         "jack": 11, "queen": 12, "king": 13}[self.__number]
+        except KeyError:
+            raise RuntimeError(f"{self.__number} is not a valid card number")
         self.__name: typing.Final[str] = f"{self.__number} of {self.__suite}"
         self.__asset_location: typing.Final[
             str
         ] = f"../assets/cards/{self.__number}_of_{self.__suite.lower()}s.png"
         super().__init__(self.__asset_location, scale=0.5, hit_box_algorithm="None")
+        self.__color: typing.Final[str] = 'black' if self.__suite.lower() in ('spade', 'club') else 'red'
         self.__is_face_up = False
 
     def get_suite(self) -> str:
@@ -74,3 +80,23 @@ class Card(arcade.Sprite):
         :return: :class:`bool`
         """
         return not self.__is_face_up
+
+    def can_be_stacked(self, other: 'Card') -> bool:
+        """
+        Returns whether the card can be stacked on the other card
+        :param other: [:class:`Card`] The other card
+        :return: [:class:`bool`] whether the card can be stacked on the other card
+        """
+        return (self.__internal_number <= other.__internal_number) and (self.__color != other.__color)
+
+    def can_be_on_foundation(self, preceding: typing.Union[None, 'Card']) -> bool:
+        """
+        Returns whether the card can be on the foundation
+        :param preceding: [:class:`Card`] the top that is on the foundation
+        :return: [:class:`bool`] whether the card can be on the foundation
+        """
+        if not preceding:
+            return True
+        return (self.__internal_number == preceding.__internal_number + 1) \
+            and (self.__color == preceding.__color) \
+            and (self.__suite == preceding.__suite)
